d7cd95c28f3a98e4c2808a3895d6fb50e48690d8
Tobias Aihonen <ta223dh@student.lnu.se>
2023-08-30 14:36:37 +0200

    Check if the name is Tobias. Colorcode the name

--- js/components/l1-row/l1-row.js
+++ js/components/l1-row/l1-row.js
@@ -23,7 +23,7 @@ customElements.define('l1-row',
         .appendChild(template.content.cloneNode(true))
 
       this.#name = this.shadowRoot.querySelector('div')
-      this.#correctName = 'Tobias'
+      this.#correctName = 'tobias'
     }
 
     static get observedAttributes () {
@@ -37,9 +37,30 @@ customElements.define('l1-row',
     }
 
     colorCodeName (name) {
-      const colorCodedName = document.createElement('span')
-      colorCodedName.textContent = name
-      this.#name.appendChild(colorCodedName)
+      let tempName = this.#correctName.toLowerCase()
+      let correct = true
+      for (let i = 0; i < name.length; i++) {
+        const colorCodedName = document.createElement('span')
+        colorCodedName.textContent = name[i]
+        if (name[i].toLowerCase() === this.#correctName[i]) {
+          colorCodedName.style.backgroundColor = 'lightgreen'
+          tempName = tempName.slice(1, tempName.length)
+        } else if (tempName.includes(name[i].toLowerCase())) {
+          colorCodedName.style.backgroundColor = 'yellow'
+          tempName = tempName.slice(0, tempName.indexOf(name[i])-1 + tempName.slice(tempName.indexOf(name[i])+1, name.length))
+          correct = false
+        } else {
+          correct = false
+        }
+        this.#name.appendChild(colorCodedName)
+      }
+      const result = document.createElement('span')
+      if (correct) {
+        result.textContent = ' ✅'
+      } else {
+        result.textContent = ' ❌'
+      }
+      this.#name.appendChild(result)
     }
   }
 )
